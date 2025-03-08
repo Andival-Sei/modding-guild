09faaf8727a5208ba975115186ed5bbc8872d614
Kirill Barkov <freedomdragon777@gmail.com>
2025-03-08 15:21:13 +0400

    Обновление структуры проекта: добавлен новый заголовок, изменены стили и обновлены зависимости

--- vite.config.js
+++ vite.config.js
@@ -1,48 +1,58 @@
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import htmlMinify from 'vite-plugin-html-minify';
-import htmlInject from 'vite-plugin-html-inject';
+// import htmlMinify from 'vite-plugin-html-minify';
+// import htmlInject from 'vite-plugin-html-inject';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { readFileSync } from 'fs';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    cssCodeSplit: false,
+	build: {
+		outDir: 'dist',
+		emptyOutDir: true,
+		cssCodeSplit: false,
 	},
 	// Добавьте эти настройки для сервера
-  server: {
-    host: '0.0.0.0', // Позволяет подключаться с любого IP
-    port: 5173      // Порт (можно оставить по умолчанию)
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // настройки SCSS
-      }
-    }
-  },
+	server: {
+		host: '0.0.0.0', // Позволяет подключаться с любого IP
+		port: 5173      // Порт (можно оставить по умолчанию)
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				// настройки SCSS
+			}
+		}
+	},
 	plugins: [
 
-		htmlInject(),
 		// Минификация HTML
-    htmlMinify({
-      minify: true,
-      removeComments: true,
-      collapseWhitespace: true,
-      removeRedundantAttributes: true,
-      removeScriptTypeAttributes: true,
-      removeStyleLinkTypeAttributes: true,
-      sortClassName: true,
-      useShortDoctype: true,
-      minifyCSS: true,
-      keepClosingSlash: true
-		}),
+    // htmlMinify({
+		// 	minify: true,
+    //   removeComments: false,
+    //   collapseWhitespace: true,
+    //   removeRedundantAttributes: true,
+    //   removeScriptTypeAttributes: true,
+    //   removeStyleLinkTypeAttributes: true,
+    //   sortClassName: true,
+    //   useShortDoctype: true,
+    //   minifyCSS: true,
+    //   keepClosingSlash: true
+		// }),
 		
+		createHtmlPlugin({
+      minify: false,
+      inject: {
+        data: {
+          header: readFileSync('src/components/header/header.html', 'utf8')
+        }
+      }
+    }),
     // Копирование статических файлов
     viteStaticCopy({
       targets: [
         {
-          src: 'src/**/*.html',
+					// src: 'src/**/*.html',
+					src: 'src/*.html', // Теперь копируем только HTML файлы из корня src
           dest: './'
         },
         {
